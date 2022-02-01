7597fe4e90b187f7c087ec3a8f19f9261d729138
Faozia Islam Riha <faozia.islam.29@gmail.com>
2022-02-01 11:25:47 +0600

    swap, output

---
+++ html/function.js
@@ -0,0 +1,8 @@
+function isEven(number) {
+    if (number %2 == 0) {
+        return true;
+    }
+    return false;
+}
+const myNumber = 1641;
+const isMyNumberEven = isEven(myNumber);
---
+++ html/number.js
@@ -0,0 +1,15 @@
+// const number =  21.48204
+// const output = Math.ceil(number)
+// const output = Math.floor(number) 
+// const output = Math.round(number)
+// console.log(output) 
+
+var first = 3
+var second = 4
+
+// var temp = first
+// first = second 
+// second = temp
+// console.log (first, second)
+[first, second] = [second, first] ;
+console.log(first, second);
