d579e74ed58c7e06129ebfa60127a598dab2cf6d
Faozia Islam Riha <faozia.islam.29@gmail.com>
2022-02-01 23:56:44 +0600

    fibonacchi

--- html/number.js
+++ html/number.js
@@ -1,15 +1,5 @@
-// const number =  21.48204
-// const output = Math.ceil(number)
-// const output = Math.floor(number) 
-// const output = Math.round(number)
-// console.log(output) 
-
-var first = 3
-var second = 4
-
-// var temp = first
-// first = second 
-// second = temp
-// console.log (first, second)
-[first, second] = [second, first] ;
-console.log(first, second);
+var  fibo = [ 1, 2, 5, 9, 13, 56]
+for (let i=0; i <= fibo.length; i++) {
+    fibo[i] = fibo[i-1] + fibo[i-2]
+ }
+ console.log(fibo)
