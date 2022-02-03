cc2a0412096fc852e09476afa093a5e81d887f06
Faozia Islam Riha <faozia.islam.29@gmail.com>
2022-02-03 13:01:23 +0600

    function

--- html/number.js
+++ html/number.js
@@ -1,5 +1,14 @@
-var  fibo = [ 1, 2, 5, 9, 13, 56]
-for (let i=0; i <= fibo.length; i++) {
-    fibo[i] = fibo[i-1] + fibo[i-2]
- }
- console.log(fibo)
+// var  fibo = [ 1, 2, 5, 9, 13, 56]
+// for (let i=0; i <= fibo.length; i++) {
+//     fibo[i] = fibo[i-1] + fibo[i-2]
+//  }
+//  console.log(fibo)
+
+function smallestElement (numbers) {
+    let min = numbers[0]
+    for (let i=0; i < numbers.length; i++)
+    const element = numbers[i]
+    if (element < min) [
+        min = element 
+    ]
+}
